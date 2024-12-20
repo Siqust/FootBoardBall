1a4a68d4f8fc8fe4c94f3f22e1046144cae7473d
Siqust <Alexasey2009@mail.ru>
2024-12-20 11:49:51 +0300

    color change

--- Assets/scripts/multiplayer/Game/PlayerCards/create.py
+++ Assets/scripts/multiplayer/Game/PlayerCards/create.py
@@ -1,18 +1,30 @@
 mx=13
 mn=3
 attack=[]
-
+import colorsys
 for i in range(12):
     attack.append(round(mx - (mx-mn)/12*i))
     print(attack[i])
 defence=list(reversed(attack))
 
+colors=[] #40-25 225-210
+mx,mn=35,10
+for i in range(6):
+    val=mn+(mx-mn)/5*i
+    x=colorsys.hsv_to_rgb(val/360,1,1)
+    print(val)
+    colors.append((x[0],x[1],x[2]))
+mx,mn=225,200
+tmp=[]
+for i in range(6):
+    val=mx-(mx-mn)/5*i
+    x=colorsys.hsv_to_rgb(val/360,1,1)
+    print(val)
+    tmp.append((x[0],x[1],x[2]))
+colors+=list(reversed(tmp))
+
 gen=True
 if gen:
-    from colour import Color
-    colors=list(Color('blue').range_to(Color("orange"),12))
-    colors=list(reversed(colors))
-
     text='''%YAML 1.1
 %TAG !u! tag:unity3d.com,2011:
 --- !u!114 &11400000
@@ -39,7 +51,7 @@ MonoBehaviour:
         name=f'P{i+1}'
         with open(name+'.asset','w') as file:
             file.write(text.replace("{ATTACK}",a).replace("{DEFENCE}",d).replace("{NAME}",name)
-                       .replace('{R}',str(colors[i].get_red())).replace('{G}',str(colors[i].get_green())).replace('{B}',str(colors[i].get_blue())))
+                       .replace('{R}',str(colors[i][0])).replace('{G}',str(colors[i][1])).replace('{B}',str(colors[i][2])))
         
 
 
